b9501707ccd5f17cd23157be0a4087badfd7cad6
Abigail <xxoliwerxx@gmail.com>
2024-10-05 22:43:18 +0200

    Fix Sliver of Straw teeth bug

--- StatusEffects/InstantDoubleZap.cs
+++ StatusEffects/InstantDoubleZap.cs
@@ -2,8 +2,10 @@
 {
     internal class InstantDoubleZap() : AbstractStatus<StatusEffectInstantDoubleX>(
         Name,
-        subscribe: status => status.statusToDouble = Rainfrost.GetStatus<StatusEffectData>(Zap.Name)
-        )
+        subscribe: status => {
+            status.statusToDouble = Rainfrost.GetStatus<StatusEffectData>(Zap.Name);
+            status.countsAsHit = false;
+        })
     {
         public const string Name = "Instant Double Zap";
     }
